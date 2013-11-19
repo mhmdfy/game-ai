a30f26990b32eaee3021a0eeadb5f9228ff7191d
mhmdfy <mhmdfy@gmail.com>
2013-11-19 16:32:56 -0500

    changed constants to 21

--- GameAIFinal/src/Constants.java
+++ GameAIFinal/src/Constants.java
@@ -2,7 +2,7 @@
 public class Constants {
 	public final static String MAP_PATH = "./maps/";
 	public final static String MAP_EXT = ".map";
-	public final static int WIDTH = 20;
-	public final static int LENGTH = 20;
+	public final static int WIDTH = 21;
+	public final static int HIEGHT = 21;
 	
 }
