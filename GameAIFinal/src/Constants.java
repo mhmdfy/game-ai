a84673a6b2eb673faa8c7c4d2bf1198fda13b2d0
mhmdfy <mhmdfy@gmail.com>
2013-11-19 09:50:03 -0500

    initial implementation of parser and main

---
+++ GameAIFinal/src/Constants.java
@@ -0,0 +1,8 @@
+
+public class Constants {
+	public final static String MAP_PATH = "./maps/";
+	public final static String MAP_EXT = ".map";
+	public final static int WIDTH = 20;
+	public final static int LENGTH = 20;
+	
+}
