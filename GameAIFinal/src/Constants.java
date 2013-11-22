53d846edf4bd14446a1145c0cf8d886eec1c5f39
mhmdfy <mhmdfy@gmail.com>
2013-11-22 10:29:59 -0500

    removing old files

--- GameAIFinal/src/Constants.java
+++
@@ -1,8 +0,0 @@
-
-public class Constants {
-	public final static String LVL_PATH = "./levels/";
-	public final static String LVL_EXT = ".lvl";
-	public final static int WIDTH = 21;
-	public final static int HEIGHT = 21;
-	
-}
