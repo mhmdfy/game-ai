5cb8e6125359ea3658ed600ce90e94928d1dd6ce
Ubuntu 32bit <justin@ubuntu.(none)>
2013-12-06 13:29:49 -0800

    outer room loop and sprites

--- GameAIFinal/src/com/level_handling/generator/RoomLevel.java
+++ GameAIFinal/src/com/level_handling/generator/RoomLevel.java
@@ -15,14 +15,17 @@ public class RoomLevel
 	{
 		Random random = new Random();
 		Level level = new Level();
-		for(int i = 0; i < Constants.WIDTH; i+=5)
+		for(int i = 0; i < Constants.WIDTH - 1; i+=5)
 		{
-			for(int j = 0; j < Constants.HEIGHT; j+=5)
+			for(int j = 0; j < Constants.HEIGHT -1; j+=5)
 			{
-				
+				createRoom(i, j, level);
+				while(!isValid(level))
+				{
+					createRoom(i, j, level);
+				}
 			}
 		}
 		return level;
-		
 	}
 }
