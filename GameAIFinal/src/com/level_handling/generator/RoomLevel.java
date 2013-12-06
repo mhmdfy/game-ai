92e0699960c54998ef6b8a54dad724e1a52f4e12
mhmdfy <mhmdfy@gmail.com>
2013-12-06 16:30:41 -0500

    created createRoom method

--- GameAIFinal/src/com/level_handling/generator/RoomLevel.java
+++ GameAIFinal/src/com/level_handling/generator/RoomLevel.java
@@ -2,18 +2,16 @@ package com.level_handling.generator;
 
 import java.util.Random;
 
-import com.data_structure.block.BreakableBlock;
-import com.data_structure.block.EmptyBlock;
 import com.data_structure.block.WallBlock;
 import com.game.Constants;
 import com.level_handling.Level;
-import com.level_handling.generator.Generator;
 
 public class RoomLevel 
 {
+	private static int roomlength = 5;
+	
 	public static Level create()
 	{
-		Random random = new Random();
 		Level level = new Level();
 		for(int i = 0; i < Constants.WIDTH; i+=5)
 		{
@@ -23,6 +21,33 @@ public class RoomLevel
 			}
 		}
 		return level;
+	}
+	
+	private static Level createRoom(int startX, int startY, Level level)
+	{
+		Level newLevel = level;
+		Random random = new Random();
 		
+		int maxDoors = 2;
+		int doorCount = 0;
+		for (int x = startX; x < startX + roomlength; x++)
+		{
+			for (int y = startY; y < startY + roomlength; y++)
+			{
+				if (!level.getBlock(x, y).isWall())
+				{
+					int putDoor = random.nextInt(9);
+					if (putDoor != 0 || doorCount == maxDoors)
+						newLevel.setBlock(x, y, new WallBlock());
+					else
+						doorCount = doorCount + 1;
+						
+				}
+			}
+		}
+		if (Generator.isValid(newLevel))
+			return newLevel;
+		else
+			return createRoom(startX, startY, level);
 	}
 }
